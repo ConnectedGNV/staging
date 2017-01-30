2adc97b8b30b375f12096f22ed2781b9a8c38b1f
Chris Dolphin <dolphin@likethemammal.com>
2017-01-30 14:13:46 -0500

    Get rid of Mailchimp Ajax functionality, forward to Mailchimp page

--- mailing-list.js
+++ mailing-list.js
@@ -1,16 +1,16 @@
 
-const $modal = $('#modal')
-const $loading = $('#state-loading')
-const $success = $('#state-success')
-const $error = $('#state-error')
+// var $modal = $('#modal')
+// var $loading = $('#state-loading')
+// var $success = $('#state-success')
+// var $error = $('#state-error')
 
-const button = document.getElementById('alt-cta-button')
-const form = document.getElementById('mailing')
-const email = document.getElementById('alt-cta-email')
-const fname = document.getElementById('alt-cta-first-name')
-const lname = document.getElementById('alt-cta-last-name')
+var button = document.getElementById('alt-cta-button')
+var form = document.getElementById('mailing')
+var email = document.getElementById('alt-cta-email')
+var fname = document.getElementById('alt-cta-first-name')
+var lname = document.getElementById('alt-cta-last-name')
 
-const validateInput = function () {
+var validateInput = function () {
     if (validateEmail(email.value) && fname.value && lname.value) {
         button.disabled = false;
     } else {
@@ -25,52 +25,71 @@ fname.onchange = validateInput
 lname.oninput = validateInput
 lname.onchange = validateInput
 
-const hideModal = (time) => {
-    setTimeout(function() {
-        $modal.hide()
-    }, time)
-}
-
-form.onsubmit = (ev) => {
+form.onsubmit = function(ev) {
     ev.preventDefault()
 
-    $modal.show()
-    $loading.show()
+    var mailingListURl = 'http://connectgnv.us14.list-manage1.com/subscribe?u=6e9bd095d4e4fbd9ebb1d63da&id=611307f043'
 
-    const $form = $(form)
+    var fieldString = params({
+        'MERGE0': email.value,
+        'MERGE1': fname.value,
+        'MERGE2': lname.value
+    })
 
-    $.ajax({
-        type: 'GET',
-        url: $form.attr('action'),
-        data: $form.serialize(),
-        cache       : false,
-        dataType: "jsonp",
-        jsonp: "c", // trigger MailChimp to return a JSONP response
-        contentType: "application/json; charset=utf-8",
-        error       : function(err) {
-            $error.show()
-            hideModal(4000)
-        },
-        success: function(data) {
-            if (data.result !== "success") {
-                console.log('error: ', data.msg)
+    var url = mailingListURl + '&' + fieldString
 
-                $loading.hide()
-                $error.show()
-                hideModal(4000)
-            } else {
-                console.log('success')
+    window.open(url)
 
-                email.value = ''
-                fname.value = ''
-                lname.value = ''
+}
 
-                $loading.hide()
-                $success.show()
-                hideModal(6000)
-            }
+// form.onsubmit = (ev) => {
+//     ev.preventDefault()
+//
+//     $modal.show()
+//     $loading.show()
+//
+//
+//     $.ajax({
+//         type: 'GET',
+//         url: $form.attr('action'),
+//         data: $form.serialize(),
+//         cache       : false,
+//         dataType: "jsonp",
+//         jsonp: "c", // trigger MailChimp to return a JSONP response
+//         contentType: "application/json; charset=utf-8",
+//         error       : function(err) {
+//             $error.show()
+//             hideModal(4000)
+//         },
+//         success: function(data) {
+//             if (data.result !== "success") {
+//                 console.log('error: ', data.msg)
+//
+//                 $loading.hide()
+//                 $error.show()
+//                 hideModal(4000)
+//             } else {
+//                 console.log('success')
+//
+//                 email.value = ''
+//                 fname.value = ''
+//                 lname.value = ''
+//
+//                 $loading.hide()
+//                 $success.show()
+//                 hideModal(6000)
+//             }
+//         }
+//     });
+// }
+
+function params(obj) {
+    var str = [];
+    for(var p in obj)
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
         }
-    });
+    return str.join("&");
 }
 
 function validateEmail(email) {
